9ff088f43963ed6d29f02b3eb5c79dadb753eff3
vitorjaum <vitorjaum711@gmail.com>
2022-12-30 04:22:43 -0400

    try add Header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,28 @@
 import styled from "styled-components";
 
-export const Header = styled.header`
+const HeaderSite = styled.header`
   background-color: #0a0a23;
   color: white;
   display: flex;
-  align-content: center;
+  align-items: center;
   justify-content: center;
-  height: 10%;
+  width: 100%;
+  height: 38px;
 `;
+
+const Logo = styled.img`
+  display: block;
+  height: 24px;
+  margin: 7px 0;
+  width: auto;
+`;
+
+export const Header = () => {
+  return (
+    <HeaderSite>
+      <a href="/">
+        <Logo src="logo.jpg" alt="" />
+      </a>
+    </HeaderSite>
+  );
+};
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,9 +3,7 @@ import { Header } from "components/header";
 export const Home: React.FC = () => {
   return (
     <>
-      <Header>
-        <h1>Desafios FreeCodeCamp</h1>
-      </Header>
+      <Header />
     </>
   );
 };
