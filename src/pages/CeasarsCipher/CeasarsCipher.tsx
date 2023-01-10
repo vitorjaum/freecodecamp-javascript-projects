fa78e99ad2612f4e23520bc01b0a3dbe3a2a27c7
vitorjaum <vitorjaum711@gmail.com>
2023-01-10 04:05:31 -0400

    add: ceasars cipher solution

--- src/pages/CeasarsCipher/CeasarsCipher.tsx
+++ src/pages/CeasarsCipher/CeasarsCipher.tsx
@@ -1,5 +1,56 @@
-import { Header } from "components";
+import { Header, InputTxt, Result, Title } from "components";
+import { useState } from "react";
+import styled from "styled-components";
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px 0;
+  padding: 0.5em;
+`;
 
 export const CeasarsCipher = () => {
-  return <Header />;
+  const [result, setResult] = useState("");
+
+  function rot13(str: string) {
+    const upperStr = str.toUpperCase();
+    const strList = upperStr.split("");
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    let newStr = "";
+
+    strList.map((char) => {
+      const currentCode = char.charCodeAt(0) - 65;
+
+      if (currentCode <= 25 && currentCode >= 0) {
+        if (currentCode + 13 <= 25) {
+          newStr += alphabet[currentCode + 13];
+        } else {
+          const newCode = currentCode + 13 - 26;
+          newStr += alphabet[newCode];
+        }
+      } else {
+        newStr += char;
+      }
+    });
+
+    return newStr;
+  }
+
+  return (
+    <>
+      <Header />
+      <Main>
+        <Title>Caesars Cipher</Title>
+        <div>
+          <InputTxt
+            inputName="Word:"
+            inputHandler={(e) => setResult(rot13(e.target.value))}
+          />
+          <Result>Resultado: {result}</Result>
+        </div>
+      </Main>
+    </>
+  );
 };
