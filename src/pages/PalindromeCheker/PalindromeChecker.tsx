47ddf27127b45a624a48d1c685fe793d27e4c9eb
vitorjaum <vitorjaum711@gmail.com>
2023-01-25 18:11:19 -0400

    refactory

--- src/pages/PalindromeCheker/PalindromeChecker.tsx
+++ src/pages/PalindromeCheker/PalindromeChecker.tsx
@@ -1,48 +1,21 @@
-import { InputTxt, Title, Header } from "components";
+import { InputTxt, Title, Header, Main, Result } from "components";
 import { useState } from "react";
-import styled from "styled-components";
-
-type ResultProps = {
-  true?: string;
-  fase?: string;
-};
-
-const Main = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 10px 0;
-  padding: 0.5em;
-`;
-
-const Result = styled.div<ResultProps>`
-  background-color: grey;
-  height: 2.5em;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #f5f6f7;
-  border: 3px solid;
-  margin: 0.5em 0;
-  ${(props) => props.true}
-`;
 
 export const PalindromeChecker = () => {
   const [result, setResult] = useState("");
-  const [newStyle, setNewStyle]: any = useState();
+  const [newStyle, setNewStyle] = useState("");
 
   const checkWord = (str: string) => {
     const word = str.toLowerCase().match(/[a-z0-9]/gi);
     const reverseWord = word && [...word]?.reverse();
 
-    str == "" && setNewStyle(`background-color: grey`);
+    str == "" && setNewStyle(`default`);
     word?.map((char, idx) => {
       if (char !== reverseWord?.[idx]) {
         setResult("Palindromen't");
-        setNewStyle(`background-color: #ffadad; color: #850000;`);
+        setNewStyle("redMessage");
       } else {
-        setNewStyle(`background-color: #cbffad; color: #008500;`);
+        setNewStyle("greenMessage");
         setResult("Palindrome");
       }
     });
@@ -55,11 +28,11 @@ export const PalindromeChecker = () => {
         <Title>Palindrome checker:</Title>
         <div>
           <InputTxt
-            inputName={"Palavra"}
-            inputHandler={(e: any) => checkWord(e.target.value)}
+            inputName={"Palavra:"}
+            inputHandler={(e) => checkWord(e.target.value)}
           />
-          <Result true={newStyle}>
-            <p>{result}</p>
+          <Result messageTheme={newStyle}>
+            <p>Result:{result}</p>
           </Result>
         </div>
       </Main>
